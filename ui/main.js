ff28671d74ea476d3b4e78aa70962856a708567b
HarshavardhanShitole <30401246+HarshavardhanShitole@users.noreply.github.com>
2017-08-06 14:36:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginleft = marginLeft + 'px';
+}
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100)
+    var interval = setInterval(moveRight, 100);
   // img.style.marginleft = '100px';  
 };
