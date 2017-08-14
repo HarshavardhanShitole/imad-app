6f2673f406f96e2eab0ceaf68d5c25275f2cbd9c
HarshavardhanShitole <30401246+HarshavardhanShitole@users.noreply.github.com>
2017-08-14 20:24:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,13 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New Value';
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-  // img.style.marginleft = '100px';  
+//Counter code
+var button = document.getElementById("counter");
+var counter = 0;
+button.onClick = function() {
+  //Make a request to capture endpoint
+  
+  //Capture the response and store it in a variable
+  
+  //Render the variable in the correct span
+  counter = counter + 1;
+  var span = document.getElementById("count");
+  span.innerHTML = counter.setString();
 };
