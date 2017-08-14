8c899ae95f28be7dd10fd0166c9768cc0efc9c18
HarshavardhanShitole <30401246+HarshavardhanShitole@users.noreply.github.com>
2017-08-14 21:44:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,24 @@
 //Counter code
 var button = document.getElementById("counter");
-var counter = 0;
 button.onclick = function() {
-  //Make a request to capture endpoint
+  //JS make us to Think in strange ways sometimes, we need to think about what to do when we receive the request response before we actually make the request  
+  //Create  a request object to capture endpoint
+  var request = new XMLHttpRequest();
   
   //Capture the response and store it in a variable
-  
-  //Render the variable in the correct span
-  counter = counter + 1;
-  var span = document.getElementById("count");
-  span.innerHTML = counter.toString();
+  request.onreadystatechange = function () {
+    if (request.readyState == XMLHttpRequest.DONE) {
+        //Take some action
+        if (request.status == 200) {
+            var counter = request.responseText;
+              //Render the variable in the correct span
+            var span = document.getElementById("count");
+            span.innerHTML = counter.toString();
+        }
+    }
+    //Ignore if its not done
+  };
+  //Make the request
+  request.open("GET", "http://jyoharsh.imad.hasura-app.io/counter", true);
+  request.send(null);
 };
